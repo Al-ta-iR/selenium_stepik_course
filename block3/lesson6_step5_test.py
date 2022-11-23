117658d90f1c0e158551667d59da73a0e4b7c682
Artyom Oleynik <artyom.oleynik@gmail.com>
2022-11-23 16:49:25 +0100

    ready test with the owls

--- block3/lesson6_step5_test.py
+++ block3/lesson6_step5_test.py
@@ -1,61 +1,35 @@
-# 1. Копируем фикстуру браузера из пред. шага
-# 2.Создаем фикстуру параметриз('переменная', ["урлы"])
-# 3. Создаем класс, название с Test..
-# 4. Создаем функцию, название с test...(селф, браузер, переменная с шага 2)
-# 5. Создаем переменную = str(math.log(int(time.time())))
-# 6. link = переменная 2 шага; браузер гет(link)
-# 7. Добавляем ожидание
-# 8. Находим поле для ввода ответа и send_keys туда переменную с 5 шага (можно и сразу ввести, не создавая переменную)
-# 9. Находим кнопку Отправить через  WebDriverWait...   EC.element_to_be_clickable (так как она не сразу доступна) и нажимаем на нее ( либо после шага 8 - добавьте ожидание)
-# 10. Создаем переменную селектора правильности ответа: просто откройте любой урл из списка, введите что угодно в поле и нажмите копку (неважно что неправильный ответ - селектор тот же), после ищите его в коде
-# 11. Ассерт 'правильный ответ' in переменная селектора правильности. text
-# 12. В конце иф нейм = мейн:
-#       пайтест.мейн()
-
-# https://stepik.org
-
-
-#     открыть страницу 
-#     авторизоваться на странице со своим логином и паролем (см. предыдущий шаг)
-#     ввести правильный ответ 
-#     нажать кнопку "Отправить" 
-#     дождаться фидбека о том, что ответ правильный 
-#     проверить, что текст в опциональном фидбеке полностью совпадает с "Correct!"
-# Опциональный фидбек — это текст в черном поле, как показано на скриншоте: (correct)
-
-# Правильным ответом на задачу в заданных шагах является число:
-# import time
-# import math
-# answer = math.log(int(time.time()))
-
-# Используйте маркировку pytest для параметризации и передайте в тест список ссылок в качестве параметров:
-# links = [
-#     'https://stepik.org/lesson/236895/step/1',
-#     'https://stepik.org/lesson/236896/step/1',
-#     'https://stepik.org/lesson/236897/step/1',
-#     'https://stepik.org/lesson/236898/step/1',
-#     'https://stepik.org/lesson/236899/step/1',
-#     'https://stepik.org/lesson/236903/step/1',
-#     'https://stepik.org/lesson/236904/step/1',
-#     'https://stepik.org/lesson/236905/step/1',
-# ]
-
-# Используйте осмысленное сообщение об ошибке в проверке текста, а также настройте нужные ожидания, чтобы тесты работали стабильно. 
-
-# В упавших тестах найдите кусочки послания. Тест должен падать, если текст в опциональном фидбеке не совпадает со строкой "Correct!" Соберите кусочки текста в одно предложение и отправьте в качестве ответа на это задание. 
-
-# Важно! Чтобы пройти это задание, дополнительно убедитесь в том, что у вас установлено правильное локальное время (https://time.is/ru/). Ответ для каждой задачи нужно пересчитывать отдельно, иначе они устаревают. 
-
 import pytest
+import math
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), '100'))
-# browser.find_element(By.ID, "ember224").click()
+links = [
+    'https://stepik.org/lesson/236895/step/1',
+    'https://stepik.org/lesson/236896/step/1',
+    # 'https://stepik.org/lesson/236897/step/1',
+    # 'https://stepik.org/lesson/236898/step/1',
+    # 'https://stepik.org/lesson/236899/step/1',
+    # 'https://stepik.org/lesson/236903/step/1',
+    # 'https://stepik.org/lesson/236904/step/1',
+    # 'https://stepik.org/lesson/236905/step/1',
+]
 
 
-# @pytest.mark.parametrize('language', ["ru", "en-gb"])
-def test_guest_should_see_login_link(browser):
-    link = f"http://selenium1py.pythonanywhere.com/ru/"
-    browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+class TestStepikBlock3():
+    @pytest.mark.parametrize('link', links)
+    def test_guest_should_see_login_link(self, browser, link):
+        browser.get(link)
+        time.sleep(3)
+
+        math_value = str(math.log(int(time.time())))
+        browser.find_element(By.CLASS_NAME, "ember-text-area.ember-view.textarea.string-quiz__textarea").send_keys(math_value)
+        time.sleep(3)
+        browser.find_element(By.CLASS_NAME, "submit-submission").send_keys(math_value)
+        browser.find_element(By.CLASS_NAME, "submit-submission").click()
+        time.sleep(1)
+        answer = browser.find_element(By.CLASS_NAME, "smart-hints__hint").text
+        time.sleep(2)
+        assert answer == 'Correct!', f'Ответ должен быть "Correct", получен {answer}'
+
+# python -m pytest -s -v block3/lesson6_step5_test.py
--- conftest.py
+++ conftest.py
@@ -1,25 +1,39 @@
 import pytest
 import time
+import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+env_path = os.path.join('secrets.env')
+load_dotenv(env_path)
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 
 
 @pytest.fixture(scope="module")
 def browser():
-    print("\nstart browser for test..")
+    print("\n► start browser for test..")
     options = Options()
     options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
     browser = webdriver.Firefox(executable_path=r'C:\WebDrivers\geckodriver.exe', options=options)
-    link_url = 'https://stepik.org/lesson/236895/step/1'
+    link_url = 'https://stepik.org/catalog?auth=login'
     browser.get(link_url)
-    time.sleep(2)
-    browser.find_element(By.ID, "ember32").click()
-    browser.find_element(By.ID, "id_login_email").send_keys('************')
-    browser.find_element(By.ID, "id_login_password").send_keys('************')
-    browser.find_element(By.CLASS_NAME, "sign-form__btn.button_with-loader").click()
-    time.sleep(2)
+    # time.sleep(1)
+    WebDriverWait(browser, 10).until(By.ID, "id_login_email").send_keys(EMAIL)
+    # WebDriverWait(browser, 5).until(
+    #     EC.element_to_be_clickable((By.ID, "verify"))
+    # )
+    # browser.find_element(By.ID, "id_login_email").send_keys(EMAIL)
+    browser.find_element(By.ID, "id_login_password").send_keys(PASSWORD)
+    WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "sign-form__btn.button_with-loader"))
+    ).click()
+    # browser.find_element(By.CLASS_NAME, "sign-form__btn.button_with-loader").click()
     yield browser
-    print("\nquit browser..")
+    print("\n◄ quit browser..")
     time.sleep(3)
     browser.quit()
