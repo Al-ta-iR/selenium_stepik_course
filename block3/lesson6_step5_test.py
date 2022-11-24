a9bc07c3581e1043fd6641d8504bd19b4e90cba1
Al-ta-iR <artyom.oleynik@gmail.com>
2022-11-24 16:27:59 +0100

    made test ideal 3.6.5

--- block3/lesson6_step5_test.py
+++ block3/lesson6_step5_test.py
@@ -3,16 +3,18 @@ import math
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 links = [
     'https://stepik.org/lesson/236895/step/1',
     'https://stepik.org/lesson/236896/step/1',
-    # 'https://stepik.org/lesson/236897/step/1',
-    # 'https://stepik.org/lesson/236898/step/1',
-    # 'https://stepik.org/lesson/236899/step/1',
-    # 'https://stepik.org/lesson/236903/step/1',
-    # 'https://stepik.org/lesson/236904/step/1',
-    # 'https://stepik.org/lesson/236905/step/1',
+    'https://stepik.org/lesson/236897/step/1',
+    'https://stepik.org/lesson/236898/step/1',
+    'https://stepik.org/lesson/236899/step/1',
+    'https://stepik.org/lesson/236903/step/1',
+    'https://stepik.org/lesson/236904/step/1',
+    'https://stepik.org/lesson/236905/step/1',
 ]
 
 
@@ -20,16 +22,23 @@ class TestStepikBlock3():
     @pytest.mark.parametrize('link', links)
     def test_guest_should_see_login_link(self, browser, link):
         browser.get(link)
-        time.sleep(3)
-
+        try:
+            WebDriverWait(browser, 5).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, "again-btn.white"))
+            ).click()
+        except:
+            pass
         math_value = str(math.log(int(time.time())))
-        browser.find_element(By.CLASS_NAME, "ember-text-area.ember-view.textarea.string-quiz__textarea").send_keys(math_value)
-        time.sleep(3)
-        browser.find_element(By.CLASS_NAME, "submit-submission").send_keys(math_value)
-        browser.find_element(By.CLASS_NAME, "submit-submission").click()
-        time.sleep(1)
-        answer = browser.find_element(By.CLASS_NAME, "smart-hints__hint").text
-        time.sleep(2)
+        WebDriverWait(browser, 15).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "ember-text-area.ember-view.textarea.string-quiz__textarea"))
+        ).send_keys(math_value)
+
+        WebDriverWait(browser, 5).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission"))
+        ).click()
+        answer = WebDriverWait(browser, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "smart-hints__hint"))
+        ).text
         assert answer == 'Correct!', f'Ответ должен быть "Correct", получен {answer}'
 
 # python -m pytest -s -v block3/lesson6_step5_test.py
--- conftest.py
+++ conftest.py
@@ -22,18 +22,14 @@ def browser():
     browser = webdriver.Firefox(executable_path=r'C:\WebDrivers\geckodriver.exe', options=options)
     link_url = 'https://stepik.org/catalog?auth=login'
     browser.get(link_url)
-    # time.sleep(1)
-    WebDriverWait(browser, 10).until(By.ID, "id_login_email").send_keys(EMAIL)
-    # WebDriverWait(browser, 5).until(
-    #     EC.element_to_be_clickable((By.ID, "verify"))
-    # )
-    # browser.find_element(By.ID, "id_login_email").send_keys(EMAIL)
+    WebDriverWait(browser, 5).until(
+        EC.visibility_of_element_located((By.ID, "id_login_email"))
+    ).send_keys(EMAIL)
     browser.find_element(By.ID, "id_login_password").send_keys(PASSWORD)
     WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.CLASS_NAME, "sign-form__btn.button_with-loader"))
     ).click()
-    # browser.find_element(By.CLASS_NAME, "sign-form__btn.button_with-loader").click()
+    time.sleep(1)
     yield browser
     print("\n◄ quit browser..")
-    time.sleep(3)
     browser.quit()
