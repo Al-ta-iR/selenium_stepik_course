7927e557e5a05c66fa930b4ed749de6f2f768add
Al-ta-iR <artyom.oleynik@gmail.com>
2022-11-25 16:56:15 +0100

    add information about adding options to driver

--- conftest.py
+++ conftest.py
@@ -28,3 +28,25 @@ def browser(request):
     browser.quit()
 
 # pytest -s -v --browser_name=firefox test_parser.py
+
+
+
+# _______________________________________________________________________________________________________________________________________
+# Чтобы указать язык браузера с помощью WebDriver, используйте класс Options и метод add_experimental_option, как указано в примере ниже:
+
+# from selenium.webdriver.chrome.options import Options
+
+# options = Options()
+# options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+# browser = webdriver.Chrome(options=options)
+
+# Для Firefox объявление нужного языка будет выглядеть немного иначе:
+
+# fp = webdriver.FirefoxProfile()
+# fp.set_preference("intl.accept_languages", user_language)
+# browser = webdriver.Firefox(firefox_profile=fp)
+# ______________
+# В конструктор webdriver.Chrome или webdriver.Firefox вы можете добавлять разные аргументы, расширяя возможности тестирования ваших веб-приложений: 
+# можно указывать прокси-сервер для контроля сетевого трафика или запускать разные версии браузера, указывая локальный путь к файлу браузера. 
+# Предполагаем, что эти возможности вам понадобятся позже и вы сами сможете найти настройки для этих задач.
+# _______________________________________________________________________________________________________________________________________
