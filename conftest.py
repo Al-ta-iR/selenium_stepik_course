21bc01f6831a213badc748fb8ddc771d2d57badd
Al-ta-iR <artyom.oleynik@gmail.com>
2022-11-25 16:37:46 +0100

    add option choosing browser

--- conftest.py
+++ conftest.py
@@ -6,13 +6,25 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='firefox',
+                     help="Choose browser: chrome or firefox")
 
 
-
-@pytest.fixture(scope="module")
-def browser():
-    print("\n► start browser for test..")
-    browser = webdriver.Firefox()
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    # else:
+    #     raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
-    print("\n◄ quit browser..")
+    print("\nquit browser..")
     browser.quit()
+
+# pytest -s -v --browser_name=firefox test_parser.py
